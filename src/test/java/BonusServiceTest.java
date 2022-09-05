e5db4c486a68bd182992f3ecf664ad38239e6500
Ilmar Khabibullin <ilmarhabibullin@yandex.ru>
2022-09-05 13:18:41 +0300

    Add test

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -23,9 +23,9 @@ public class BonusServiceTest {
         BonusService service = new BonusService();
 
         //Подготавливаем данные:
-        long amount = 1000;
+        long amount = 18_000;
         boolean registered = true;
-        long expected = 30;
+        long expected = 500;
 
         //Вызывваем целевой метод:
         long actual = service.calculate(amount, registered);
@@ -34,4 +34,35 @@ public class BonusServiceTest {
         Assertions.assertEquals(expected, actual);
     }
 
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForRegisteredAndForLimit(){
+        BonusService service = new BonusService();
+
+        //Подготавливаем данные:
+        long amount = 42_000;
+        boolean registered = false;
+        long expected = 420;
+
+        //Вызывваем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        //Производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForRegisteredAndInLimit(){
+        BonusService service = new BonusService();
+
+        //Подготавливаем данные:
+        long amount = 190_000;
+        boolean registered = false;
+        long expected = 500;
+
+        //Вызывваем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        //Производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+    }
 }
